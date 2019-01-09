18707dd1deb3fd35af43390b95d130803f18b6da
Emo <eiv.razgrad@gmail.com>
2019-01-09 16:27:47 +0200

    add

--- StartUp/Core/Engine/Engine.cs
+++ StartUp/Core/Engine/Engine.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using StartUp.Core.Engine.Contract;
 using StartUp.Entity;
 
@@ -42,12 +44,30 @@ namespace StartUp.Core.Engine
                     ListPerson.Add(Person);
                 }
             }
+
+            Bullettin.yes.AddRange(ListPerson.Where(x => x.Vote == Bullettin.Yes));
+            Bullettin.no.AddRange(ListPerson.Where(x => x.Vote == Bullettin.No));
+            Bullettin.pass.AddRange(ListPerson.Where(x => x.Vote == Bullettin.Pass));
+            //
+
             Console.WriteLine(Bullettin.Result);
             Console.WriteLine(Bullettin.AllVote+ListPerson.Count);
-            foreach (var person in ListPerson)
-            {
-                Console.WriteLine(person.Name+" => "+person.Vote);
-            }
+            Console.WriteLine(Bullettin.dots);
+            //
+            var percY = Bullettin.yes.Count*1.0 / ListPerson.Count*1.0;
+            Console.WriteLine($"{percY * 100:f2} %");
+
+            Bullettin.PrintY();
+
+            Bullettin.PrintN();
+            var percN = Bullettin.no.Count*1.0 / ListPerson.Count * 1.0;
+            Console.WriteLine($"{percN * 100:f2} %");
+
+
+            Bullettin.PrintP();
+            var perPass = Bullettin.pass.Count*1.0 / ListPerson.Count * 1.0;
+            Console.WriteLine($"{perPass*100:f2} %");
+
         }
     }
 }
--- StartUp/Entity/Bullettin.cs
+++ StartUp/Entity/Bullettin.cs
@@ -9,7 +9,51 @@ namespace StartUp.Entity
         public static string No = "No";
         public static string Pass = "Pass";
 
-        public static List<string> all = new List<string> { Yes, No, Pass};
+        public static List<string> all = new List<string> {Yes, No, Pass};
+
+        public static List<IPerson> yes = new List<IPerson>();
+        public static List<IPerson> no = new List<IPerson>();
+        public static List<IPerson> pass = new List<IPerson>();
+        public static List<IPerson> AllVotingPeople = new List<IPerson>();
+
+        public static int AllVField()
+        {
+            AllVotingPeople.AddRange(yes);
+            AllVotingPeople.AddRange(no);
+            AllVotingPeople.AddRange(pass);
+            return AllVotingPeople.Count;
+
+        }
+
+        private static double temp => AllVField();
+
+        public static void PrintY()
+        {
+            Console.WriteLine("Voting Yes => "+yes.Count);
+
+            foreach (var person in yes)
+            {
+                Console.WriteLine(person.Name + " => "+person.Vote);
+            }
+        }
+        public static void PrintN()
+        {
+            Console.WriteLine("Voting No => "+no.Count );
+            foreach (var person in no)
+            {
+                Console.WriteLine(person.Name + " => " + person.Vote);
+            }
+        }
+        public static void PrintP()
+        {
+            Console.WriteLine("Voting Pass => " +pass.Count);
+            foreach (var person in pass)
+            {
+                Console.WriteLine(person.Name + " => " + person.Vote);
+            }
+        }
+
+
 
         public static void Print()
         {
@@ -21,8 +65,9 @@ namespace StartUp.Entity
         public static string EnterVote = "Enter vote: ";
         public static string Invalid = "Invalid - You cant vote second time";
 
-        public static string Result = "Result";
-        public static string AllVote = "All Vote";
+        public static string dots = new string('*', 10);
+        public static string Result = "RESULT"+"\n"+dots;
+        public static string AllVote = "All Vote => ";
 
         public static string Stop = "Stop";
 
